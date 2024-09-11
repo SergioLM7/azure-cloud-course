fcf48e2259ade874445ec7b380d96e2002f2a7ce
Sergio Lillo <sergiolillom@gmail.com>
2024-09-11 11:38:45 +0200

    use of variables and print concat message in console

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -1,8 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Drawing;
 
+//lesson 0
 // Console.WriteLine("Hello World!");
 
+//lesson 1
 Console.Write("Congratulations!");
 Console.Write(" ");
-Console.Write("You wrote your first lines of code.");
+Console.WriteLine("You wrote your first lines of code.");
+
+//lesson 2
+string firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
+
+int messagesNumber = 3;
+double temperature = 34.4;
+Console.Write("Hello, " + firstName + "! You have " + messagesNumber + " messages in your inbox. The temperature is " + temperature + " celsius.");
+
+
+
