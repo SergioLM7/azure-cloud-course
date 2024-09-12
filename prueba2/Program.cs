ccc550ed0abc3486216d68f283e57fb08636fa00
Sergio Lillo <sergiolillom@gmail.com>
2024-09-12 12:54:40 +0200

    conversion farenheit to celsius and basic operations

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -1,59 +1,72 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Text;
 
-//lesson 0
-// Console.WriteLine("Hello World!");
-
-//lesson 1
-Console.Write("Congratulations!");
-Console.Write(" ");
-Console.WriteLine("You wrote your first lines of code.");
-
-//lesson 2
-string firstName = "Bob";
-Console.WriteLine(firstName);
-firstName = "Liem";
-Console.WriteLine(firstName);
-firstName = "Isabella";
-Console.WriteLine(firstName);
-firstName = "Yasmin";
-Console.WriteLine(firstName);
-
-int messagesNumber = 3;
-double temperature = 34.4;
-Console.WriteLine("Hello, " + firstName + "! You have " + messagesNumber + " messages in your inbox. The temperature is " + temperature + " celsius.");
-
 //lesson 3
-Console.WriteLine(@"http:\\www.msn.com");
+int firstNumber = 12;
+int secondNumber = 7;
+var sumNumbers = firstNumber + secondNumber;
+Console.WriteLine(sumNumbers);
 
-Console.OutputEncoding = Encoding.UTF8;
+string firstName = "Bob";
+int widgetSold = 7;
+Console.WriteLine(firstName + " sold " + widgetSold + " widgets.");
+Console.WriteLine(firstName + " sold " + widgetSold + 7 + " widgets.");
+Console.WriteLine(firstName + " sold " + (widgetSold + 7) + " widgets.");
 
+//Exercise lesson 3
+var firstNumber2 = 45;
+var secondNumber2 = 2;
+double decimalNumber1 = 4.5;
+double decimalNumber2 = 2.35;
 
-Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
-Console.WriteLine("Invoice: 1021\t\tComplete!");
-Console.WriteLine("Invoice: 1022\t\tComplete!");
-Console.WriteLine("\nOutput Directory:\t");
-Console.Write(@"c:\invoices");
+//Sum two int
+var sum = firstNumber2 + secondNumber2;
+Console.WriteLine("This is the result of a sum: " + sum);
 
-// To generate Japanese invoices:
-// Nihon no seikyū-sho o seisei suru ni wa:
-Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
-// User command to run an application
-Console.WriteLine(@"c:\invoices\app.exe -j");
+//Substr two int
+var substraction = firstNumber2 - secondNumber2;
+Console.WriteLine("This is the result of a subs: " +substraction);
 
-Console.Clear();
-string greeting = "Hello, ";
-string message = $"{greeting} {firstName}!";
-Console.WriteLine(message);
+//Mult two int
+var multiplication = firstNumber2 * secondNumber2;
+Console.WriteLine("This is the result of a mult: " + multiplication);
 
-int version = 11;
-Console.WriteLine(version.ToString());
-string updateText = "Update to Windows";
-Console.WriteLine( $"{updateText} {version}");
+//Div and decimal Div
+var div = firstNumber2 / secondNumber2;
+decimal decimalDivResult = firstNumber2 / Convert.ToDecimal(secondNumber2);
+decimal decimalDivResultV2 = firstNumber2 / (decimal)secondNumber2;
+Console.WriteLine("This is the result of a div: " + div);
+Console.WriteLine("This is the result of a decimal div: " + decimalDivResult);
+Console.WriteLine("This is the result of a decimal div V2: " + decimalDivResultV2);
 
-//Console.Clear();
-string projectName = "First-Project";
-var path = $@"C:\Output\{projectName}\Data";
-Console.WriteLine(path);
+//Div two doubles
+var decimalDiv = decimalNumber1 / decimalNumber2;
+Console.WriteLine("This is the result of a div of two doubles: " + decimalDiv);
 
-Console.WriteLine(@"\");
+//Rest of a div of int
+var restDiv = firstNumber2 % secondNumber2;
+Console.WriteLine("This is the rest of a div: " + restDiv);
+
+//Otra opcion de obtener resultados decimales de una division
+decimal a = 10 / 3m;
+Console.WriteLine(a);
+
+//Exercise 2 lesson 3
+/* int value = 0;
+ value = value + 5;
+ value += 5;
+
+Console.Write(value);*/
+
+//Exercise Farenheit to Celsius
+//Para convertir temperaturas de grados Fahrenheit a Celsius, primero reste 32 y luego multiplique por cinco novenos (5/9).
+var farenheitTemperature = 78;
+var farenheitToCelsius = (farenheitTemperature - 32) * 5/9m;
+var celsiusOneDecimal =  Math.Round(farenheitToCelsius, 1);
+Console.WriteLine(farenheitToCelsius); //25.555555555555555555555555556
+Console.WriteLine($"The temperature of {farenheitTemperature}F is {celsiusOneDecimal}º Celsius."); //The temperature of 78F is 25.6º Celsius.
+
+//Options B and C
+var celsiusOneDecimal2 = decimal.Round(farenheitToCelsius, 1);
+Console.WriteLine(celsiusOneDecimal2); //25.6
+Console.WriteLine("The temperature is " + farenheitToCelsius.ToString("##.#") + " Celsius."); //Mascara de una cadena de texto
