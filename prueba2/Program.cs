aa57c6fa433de32af13fa1725cab60cbc3525b49
Sergio Lillo <sergiolillom@gmail.com>
2024-09-16 14:00:11 +0200

    first steps lesson 8 exercise 2

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -3,27 +3,60 @@ using System;
 using System.Collections;
 
 //Lesson 8 Arrays ForEach
+//Exercise 2
+int[] sophiaScores = [90, 86, 87, 98, 100];
+int[] andrewScores = [92, 89, 81, 96, 90];
+int[] emmaScores = [90, 85, 87, 98, 68];
+int[] loganScores = [90, 95, 87, 88, 96];
 
-string[] fraudulentOrderIDs = new string[3];
+int sophiaSum = 0;
+decimal sophiaAverage;
 
-fraudulentOrderIDs[0] = "A123";
-fraudulentOrderIDs[1] = "B456";
-fraudulentOrderIDs[2] = "C789";
-//fraudulentOrderIDs[3] = "D000"; //Error because index is outside the bounds of the array
-Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
-
-string[] names = ["Rowena", "Robin", "Bao"];
-foreach (string name in names)
+foreach (var score in sophiaScores)
 {
-    Console.WriteLine(name);
+    sophiaSum += score;
 }
 
-Console.Clear();
-//Exercise 1 Lesson 8
-string[] orderIDs = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
-foreach (var ID in orderIDs)
+sophiaAverage = (decimal)sophiaSum / sophiaScores.Length;
+
+Console.WriteLine(sophiaAverage);
+
+if(sophiaSum >= 97 && sophiaSum <= 100) 
 {
-   if(ID.StartsWith('B'))
-    Console.WriteLine(ID);
-   
+    Console.WriteLine($"Sophia:\t\t{(decimal)sophiaSum / sophiaScores.Length}\tA+");
+} else if(sophiaSum >= 93 && sophiaSum <= 96) 
+{
+    Console.WriteLine($"Sophia:\t\t{(decimal)sophiaSum / sophiaScores.Length}\tA");
+} else if(sophiaSum >= 90 && sophiaSum <= 92) 
+{
+    Console.WriteLine($"Sophia:\t\t{(decimal)sophiaSum / sophiaScores.Length}\tA-");
+} else if(sophiaSum >= 87 && sophiaSum <= 89) 
+{
+    Console.WriteLine($"Sophia:\t\t{(decimal)sophiaSum / sophiaScores.Length}\tB+");
+} else if(sophiaSum >= 83 && sophiaSum <= 86) 
+{
+    Console.WriteLine($"Sophia:\t\t{(decimal)sophiaSum / sophiaScores.Length}\tB");
+} else if(sophiaSum >= 80 && sophiaSum <= 82) 
+{
+    Console.WriteLine($"Sophia:\t\t{(decimal)sophiaSum / sophiaScores.Length}\tB-");
+} else if(sophiaSum >= 77 && sophiaSum <= 79) 
+{
+    Console.WriteLine($"Sophia:\t\t{(decimal)sophiaSum / sophiaScores.Length}\tC+");
+} else if(sophiaSum >= 73 && sophiaSum <= 76) 
+{
+    Console.WriteLine($"Sophia:\t\t{(decimal)sophiaSum / sophiaScores.Length}\tC");
+} else if(sophiaSum >= 70 && sophiaSum <= 72) 
+{
+    Console.WriteLine($"Sophia:\t\t{(decimal)sophiaSum / sophiaScores.Length}\tC-");
 }
+  
+
+/*
+Console.WriteLine("Student\t\tGrade\n");
+Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
+Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
+Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();*/
