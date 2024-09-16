24255e6bcc01e5f9e72af3f367c062e0df851a6c
Sergio Lillo <sergiolillom@gmail.com>
2024-09-16 11:15:18 +0200

    Last exercise lesson 7 - loops and if / else

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -3,80 +3,47 @@ using System;
 using System.Collections;
 
 //Lesson 7 Conditionals
-//Option 1
-Random dice = new Random();
+//Last Exercise
+Random random = new Random();
+bool continua = true;
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-
-int total2 = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total2}");
-//Option 2 without sum
-Console.WriteLine($"Dice roll: {dice.Next(3, 19)}");
-//Option 3 without sum
-var total = 0;
-var tirada = 0;
-
-Console.Write($"Dice roll: ");
-for (int i = 0; i < 3; i++)
+while (continua)
 {
-    tirada = dice.Next(1, 7);
-    total += tirada;
+    Console.Clear();
 
-    if (i == 2)
+    int daysUntilExpiration = random.Next(12);
+    int discountPercentage = 0;
+    Console.WriteLine(daysUntilExpiration);
+    if (daysUntilExpiration <= 10)
     {
-        Console.Write($"{tirada}. ");
-    }
-    else
-    {
-        Console.Write($"{tirada} + ");
-    }
-
-}
-Console.WriteLine($"Total: {total}");
-//Conditional to resolve if you win or lose
-if (total >= 14)
-{
-    Console.WriteLine("You win!");
-}
-else
-{
-    Console.WriteLine("Sorry, you lose.");
-}
-
-//Console.WriteLine('a' > 'c');
-
-//Option 4 with an array
-var total3 = 0;
-int[] tirada3 = new int[3];
-
-Console.Write($"Dice roll: ");
-for (int i = 0; i < 3; i++)
-{
-    tirada3[i] = dice.Next(1, 7);
-    total3 += tirada3[i];
-    if (i == 2)
-    {
-        Console.Write($"{tirada3[i]}. ");
-    }
-    else
-    {
-        Console.Write($"{tirada3[i]} + ");
+        if (daysUntilExpiration <= 5)
+        {
+            if (daysUntilExpiration == 1)
+            {
+                discountPercentage = 20;
+                Console.WriteLine($"Your subscription expires within a day!\nRenew now and save {discountPercentage}%!");
+            }
+            else if (daysUntilExpiration < 1)
+            {
+                Console.WriteLine("Your subscription has expired.");
+            }
+            else
+            {
+                discountPercentage = 10;
+                Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save {discountPercentage}%!.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Your subscription will expire soon. Renew now!");
+        }
     }
 
+    //Pulsando una tecla en la consola me vuela a generar un nuevo valor random hasta que pulso la q del teclado, que me saca del bucle
+    Console.WriteLine("\nPress any key to generate a new random value, or 'q' to quit.");
+    var key = Console.ReadKey();
+     if (key.KeyChar == 'q')
+            {
+                continua = false;
+            }
 }
-
-//Comprueba si la tirada ha sido triple y si no, si ha sido doble
-if (tirada3[0] == tirada3[1] && tirada3[1] == tirada3[2])
-{
-    Console.WriteLine("You rolled triples! +6 bonus to total!");
-    total += 6;
-}
-else if ((tirada3[0] == tirada3[1]) || (tirada3[1] == tirada3[2]) || (tirada3[0] == tirada3[2]))
-{
-    Console.Write("You rolled doubles! +2 bonus to total! ");
-    total3 += 2;
-}
-Console.WriteLine($"Total: {total3}");
