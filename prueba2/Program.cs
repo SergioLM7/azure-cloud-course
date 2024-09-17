7b06548adcb7043a20c65b0abf931f6037fbe458
Sergio Lillo <sergiolillom@gmail.com>
2024-09-17 12:51:30 +0200

    exercise with switch case

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -5,151 +5,78 @@ using System.Collections.ObjectModel;
 using System.Net.Http.Headers;
 using System.Text;
 
-//Lesson 8 Arrays ForEach
-//Exercise 2
-int[] sophiaScores = [90, 86, 87, 98, 100];
-int[] andrewScores = [92, 89, 81, 96, 90];
-int[] emmaScores = [90, 85, 87, 98, 68];
-int[] loganScores = [90, 95, 87, 88, 96];
-string[] studentNames = ["Sophia", "Andrew", "Emma", "Logan"];
+//Lesson 9 Switch-case
+//Exercise 1
+// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
 
-int[] studentScores = new int[10];
+string[] product = sku.Split('-');
 
-Console.WriteLine("Student\t\tGrade");
+string type = "";
+string color = "";
+string size = "";
 
-foreach (var student in studentNames)
-{
-    string currentStudent = student;
-
-    if (student == "Sophia")
-        studentScores = sophiaScores;
-    else if (currentStudent == "Andrew")
-        studentScores = andrewScores;
-    else if (currentStudent == "Emma")
-        studentScores = emmaScores;
-    else if (currentStudent == "Logan")
-        studentScores = loganScores;
-
-    int sumScores = 0;
-    decimal curentStudentAverage;
-
-    foreach (int score in studentScores)
-    {
-        sumScores += score;
-    }
-
-    curentStudentAverage = (decimal)sumScores / studentScores.Length;
-
-    if (curentStudentAverage >= 97 && curentStudentAverage <= 100)
-    {
-        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tA+");
-    }
-    else if (curentStudentAverage >= 93 && curentStudentAverage < 97)
-    {
-        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tA");
-    }
-    else if (curentStudentAverage >= 90 && curentStudentAverage < 93)
-    {
-        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tA-");
-    }
-    else if (curentStudentAverage >= 87 && curentStudentAverage < 90)
-    {
-        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tB+");
-    }
-    else if (curentStudentAverage >= 83 && curentStudentAverage < 87)
-    {
-        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tB+");
-    }
-    else if (curentStudentAverage >= 80 && curentStudentAverage < 83)
-    {
-        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tB-");
-    }
-    else if (curentStudentAverage >= 77 && curentStudentAverage < 80)
-    {
-        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tC+");
-    }
-    else if (curentStudentAverage >= 73 && curentStudentAverage < 77)
-    {
-        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tC");
-    }
-    else if (curentStudentAverage >= 70 && curentStudentAverage < 73)
-    {
-        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tC-");
-    }
-    else
-    {
-        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tF");
-    }
-
+switch (product[0]) {
+    case "01":
+    type = "Sweat shirt";
+    break;
+    case "02":
+     type = "T-Shirt";
+    break;
+    case "03":
+     type = "Sweat pants";
+    break;
+    default:
+    type = "Other";
+    break;
 }
 
-Console.WriteLine("\nPress the Enter key to continue");
-Console.ReadLine();
-
-/*
-Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
-Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
-
-Console.WriteLine("Press the Enter key to continue");
-Console.ReadLine();*/
-//Option B - classroom
-string[] alumnos = { "Sophia", "Andrew", "Emma", "Logan", "Sixto", "Juana" };
-
-// Crear un array de arrays de enteros
-//Inicializar directamente
-int[][] notas = new int[][]
+switch (product[1])
 {
-new int[] { 90, 86, 87, 98, 100 },
-new int[] { 92, 89, 81, 96, 90 },
-new int[] { 90, 85, 87, 98, 68 },
-new int[] { 90, 95, 87, 88, 96 },
-new int[] { 97, 93, 87, 82, 86 }
-};
-
-if (alumnos.Length != notas.Length)
-{
-    if (alumnos.Length > notas.Length) {
-        Console.WriteLine("Hay mas alumnos registrados que notas.");
-    } else {
-        Console.WriteLine("Hay mas notas registradas que alumnos.");
-    }
-}
-else
-{
-    var indiceDeAlumno = 0;
-    decimal sumaDeValores = 0;
-    // Mostrar los nombres y sus valores asociados
-    foreach (var alumno in alumnos)
-    {
-        sumaDeValores = notas[indiceDeAlumno].Sum();
-
-        var media = sumaDeValores / notas[indiceDeAlumno].Length;
-        indiceDeAlumno++;
-        sumaDeValores = 0;
-
-
-        var notaEnLetra = "F";
-        if (media <= 59) { notaEnLetra = "F"; }
-        else if (media <= 62) { notaEnLetra = "D-"; }
-        else if (media <= 66) { notaEnLetra = "D"; }
-        else if (media <= 69) { notaEnLetra = "D+"; }
-        else if (media <= 72) { notaEnLetra = "C-"; }
-        else if (media <= 76) { notaEnLetra = "C"; }
-        else if (media <= 79) { notaEnLetra = "C+"; }
-        else if (media <= 82) { notaEnLetra = "B-"; }
-        else if (media <= 86) { notaEnLetra = "B"; }
-        else if (media <= 89) { notaEnLetra = "B+"; }
-        else if (media <= 92) { notaEnLetra = "A-"; }
-        else if (media <= 96) { notaEnLetra = "A"; }
-        else if (media <= 100) { notaEnLetra = "A+"; }
-        else { notaEnLetra = "Out Range"; }
-
-        Console.WriteLine($"Nombre: {alumno} \t Valores: {media} \t {notaEnLetra}");
-    }
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
 }
 
-Console.WriteLine("Press the Enter key to continue");
-Console.ReadLine();
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
+
+//Boolean expressions
+Console.WriteLine("a" == "a");
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
+
+string myValue = "a";
+Console.WriteLine(myValue == "a");
+
+string value1 = " a";
+string value2 = "A ";
+//ToLower lo pasa todo a minisculas y el trim quita espacios en blanco por delante y por detras
+Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+
+//Contains es un metodo de String que evalua si la cadena de texto que analizamos contiene los caracteres que le paso
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));
