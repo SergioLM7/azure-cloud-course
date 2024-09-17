b669e693796b270e51e9140577b29d1ce192477f
SergioLM7 <sergiolillom@gmail.com>
2024-09-17 09:45:26 +0200

    Exercise 2 Lesson 8 foreach anda arrays DONE

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 using System.Collections;
+using System.Collections.ObjectModel;
 
 //Lesson 8 Arrays ForEach
 //Exercise 2
@@ -8,48 +9,81 @@ int[] sophiaScores = [90, 86, 87, 98, 100];
 int[] andrewScores = [92, 89, 81, 96, 90];
 int[] emmaScores = [90, 85, 87, 98, 68];
 int[] loganScores = [90, 95, 87, 88, 96];
+string[] studentNames = ["Sophia", "Andrew", "Emma", "Logan"];
 
-int sophiaSum = 0;
-decimal sophiaAverage;
+int[] studentScores = new int[10];
 
-foreach (var score in sophiaScores)
+Console.WriteLine("Student\t\tGrade");
+
+foreach (var student in studentNames)
 {
-    sophiaSum += score;
+    string currentStudent = student;
+
+    if (student == "Sophia")
+        studentScores = sophiaScores;
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    int sumScores = 0;
+    decimal curentStudentAverage;
+
+    foreach (int score in studentScores)
+    {
+        sumScores += score;
+    }
+
+    curentStudentAverage = (decimal)(sumScores) / studentScores.Length;
+
+    if (curentStudentAverage >= 97 && curentStudentAverage <= 100)
+    {
+        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tA+");
+    }
+    else if (curentStudentAverage >= 93 && curentStudentAverage < 97)
+    {
+        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tA");
+    }
+    else if (curentStudentAverage >= 90 && curentStudentAverage < 93)
+    {
+        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tA-");
+    }
+    else if (curentStudentAverage >= 87 && curentStudentAverage < 90)
+    {
+        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tB+");
+    }
+    else if (curentStudentAverage >= 83 && curentStudentAverage < 87)
+    {
+        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tB+");
+    }
+    else if (curentStudentAverage >= 80 && curentStudentAverage < 83)
+    {
+        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tB-");
+    }
+    else if (curentStudentAverage >= 77 && curentStudentAverage < 80)
+    {
+        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tC+");
+    }
+    else if (curentStudentAverage >= 73 && curentStudentAverage < 77)
+    {
+        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tC");
+    }
+    else if (curentStudentAverage >= 70 && curentStudentAverage < 73)
+    {
+        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tC-");
+    }
+    else
+    {
+        Console.WriteLine($"{currentStudent}\t\t{curentStudentAverage}\tF");
+
+    }
+
 }
 
-sophiaAverage = (decimal)sophiaSum / sophiaScores.Length;
-
-Console.WriteLine(sophiaAverage);
-
-if(sophiaSum >= 97 && sophiaSum <= 100) 
-{
-    Console.WriteLine($"Sophia:\t\t{(decimal)sophiaSum / sophiaScores.Length}\tA+");
-} else if(sophiaSum >= 93 && sophiaSum <= 96) 
-{
-    Console.WriteLine($"Sophia:\t\t{(decimal)sophiaSum / sophiaScores.Length}\tA");
-} else if(sophiaSum >= 90 && sophiaSum <= 92) 
-{
-    Console.WriteLine($"Sophia:\t\t{(decimal)sophiaSum / sophiaScores.Length}\tA-");
-} else if(sophiaSum >= 87 && sophiaSum <= 89) 
-{
-    Console.WriteLine($"Sophia:\t\t{(decimal)sophiaSum / sophiaScores.Length}\tB+");
-} else if(sophiaSum >= 83 && sophiaSum <= 86) 
-{
-    Console.WriteLine($"Sophia:\t\t{(decimal)sophiaSum / sophiaScores.Length}\tB");
-} else if(sophiaSum >= 80 && sophiaSum <= 82) 
-{
-    Console.WriteLine($"Sophia:\t\t{(decimal)sophiaSum / sophiaScores.Length}\tB-");
-} else if(sophiaSum >= 77 && sophiaSum <= 79) 
-{
-    Console.WriteLine($"Sophia:\t\t{(decimal)sophiaSum / sophiaScores.Length}\tC+");
-} else if(sophiaSum >= 73 && sophiaSum <= 76) 
-{
-    Console.WriteLine($"Sophia:\t\t{(decimal)sophiaSum / sophiaScores.Length}\tC");
-} else if(sophiaSum >= 70 && sophiaSum <= 72) 
-{
-    Console.WriteLine($"Sophia:\t\t{(decimal)sophiaSum / sophiaScores.Length}\tC-");
-}
-  
+Console.WriteLine("\nPress the Enter key to continue");
+Console.ReadLine();
 
 /*
 Console.WriteLine("Student\t\tGrade\n");
