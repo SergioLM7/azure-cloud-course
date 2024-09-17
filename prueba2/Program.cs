a05e65eb492578fa6238f7bd2cdd01d08ff404d7
Sergio Lillo <sergiolillom@gmail.com>
2024-09-17 13:52:45 +0200

    Exercise 3 lesson 9 ternary operators

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -5,78 +5,53 @@ using System.Collections.ObjectModel;
 using System.Net.Http.Headers;
 using System.Text;
 
-//Lesson 9 Switch-case
-//Exercise 1
-// SKU = Stock Keeping Unit. 
-// SKU value format: <product #>-<2-letter color code>-<size code>
-string sku = "01-MN-L";
+//Lesson 9 Ternary operator
+//Exercise 2
+Random coin = new();
+int flip = coin.Next(1, 3);
+Console.WriteLine(flip == 1 ? "heads" : "tails");
 
-string[] product = sku.Split('-');
+//Exercise 2 version 1 line
+Console.WriteLine(new Random().Next(1, 3) == 1 ? "heads" : "tails");
 
-string type = "";
-string color = "";
-string size = "";
+/*Use la clase Random para generar un valor.
 
-switch (product[0]) {
-    case "01":
-    type = "Sweat shirt";
-    break;
-    case "02":
-     type = "T-Shirt";
-    break;
-    case "03":
-     type = "Sweat pants";
-    break;
-    default:
-    type = "Other";
-    break;
-}
+Tenga en cuenta el intervalo de números necesarios.
 
-switch (product[1])
+Según el valor generado, use el operador condicional para mostrar heads o tails.
+
+Debería haber una posibilidad del 50 % de que el resultado sea cara o cruz.
+
+El código debe ser fácil de leer, pero con el menor número posible de líneas.
+
+Debería lograr el resultado deseado en tres líneas de código o menos.*/
+
+//Exercise 3 version 1 line
+string permission = "Admin"; //Admin or Manager
+int level = 30;
+
+if (permission.Contains("Admin"))
 {
-    case "BL":
-        color = "Black";
-        break;
-    case "MN":
-        color = "Maroon";
-        break;
-    default:
-        color = "White";
-        break;
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else if (level <= 55)
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
 }
-
-switch (product[2])
+else if (permission.Contains("Manager"))
 {
-    case "S":
-        size = "Small";
-        break;
-    case "M":
-        size = "Medium";
-        break;
-    case "L":
-        size = "Large";
-        break;
-    default:
-        size = "One Size Fits All";
-        break;
+    if (level > 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else if (level <= 20)
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+} else {
+    Console.WriteLine("You do not have sufficient privileges.");
 }
 
-Console.WriteLine($"Product: {size} {color} {type}");
-
-//Boolean expressions
-Console.WriteLine("a" == "a");
-Console.WriteLine("a" == "A");
-Console.WriteLine(1 == 2);
-
-string myValue = "a";
-Console.WriteLine(myValue == "a");
-
-string value1 = " a";
-string value2 = "A ";
-//ToLower lo pasa todo a minisculas y el trim quita espacios en blanco por delante y por detras
-Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
-
-//Contains es un metodo de String que evalua si la cadena de texto que analizamos contiene los caracteres que le paso
-string pangram = "The quick brown fox jumps over the lazy dog.";
-Console.WriteLine(pangram.Contains("fox"));
-Console.WriteLine(pangram.Contains("cow"));
