bda787847e89df6d89a27b204c86daa867ab9bcc
Sergio Lillo <sergiolillom@gmail.com>
2024-09-19 14:07:12 +0200

    Exercise 1 Lesson 11 array methods

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -7,34 +7,20 @@ using System.Text;
 using System.Globalization;
 CultureInfo.CurrentCulture = new CultureInfo("es-ES");
 
-//Lesson 10 Variables converssion
+//Lesson 11 Array Methods
 //Exercise 1
-string[] values = { "12,3", "45", "ABC", "11", "DEF" };
-string message = "";
-double total = 0;
-
-foreach (var value in values)
+string pangram = "The quick brown fox jumps over the lazy dog";
+string [] pangramArray = pangram.Split(' ');
+string pangramReversed = "";
+foreach (var word in pangramArray)
 {
-    if (double.TryParse(value, out double result)) {
-        total += result;
-    } else {
-        message += value;
-    }
+    //Console.WriteLine(word);
+    char[] letters = word.ToCharArray();
+    Array.Reverse(letters);
+    var wordReversed = String.Join("", letters);
+    //Console.WriteLine(wordReversed);
+    pangramReversed += wordReversed + " ";
+
 }
 
-Console.WriteLine("Message: " + message); 
-Console.WriteLine("Total: " + total);
-
-//Exercise 2
-int value1 = 11;
-decimal value2 = 6.2m;
-float value3 = 4.3f;
-
-decimal result1 = (decimal) value1 / value2;
-Console.WriteLine($"Divide value1 by value2, display the result as an int: {Math.Round(result1)}");
-
-decimal result2 = value2 / (decimal)value3;
-Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
-
-float result3 = value3 / value1;
-Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+ Console.WriteLine(pangramReversed);
