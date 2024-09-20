ddbb12f405229ba2f721bd723d1d34f5e8742b45
Sergio Lillo <sergiolillom@gmail.com>
2024-09-20 14:00:22 +0200

    DateTimes

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -7,21 +7,13 @@ using System.Text;
 using System.Globalization;
 CultureInfo.CurrentCulture = new CultureInfo("en-US");
 
-//Lesson 12 String Methods - IndexOf and Substring
-//Exercise 1
-const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+//Lesson 12 DateTime
+Console.WriteLine(DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss.fff"));
+Console.WriteLine(DateTime.UtcNow.ToShortTimeString());
+Console.WriteLine(DateTime.UtcNow.AddHours(2).ToShortTimeString());
 
-string quantity = "";
-string output = "";
+var date = DateTime.UtcNow;
+Console.WriteLine(date.AddDays(31));
 
-var indexOfSpan = input.IndexOf("<span>");
-var lastIndexOfSpan = input.IndexOf("</span>");
-var length = lastIndexOfSpan - (indexOfSpan+6);
-quantity = input.Substring(indexOfSpan+6,length);
-var indexOfDiv = input.IndexOf("<div>");
-var lastIndexOfDiv = input.IndexOf("</div>");
-var length2 = lastIndexOfDiv - (indexOfDiv+5);
-output = input.Substring(indexOfDiv+5,length2).Replace("&trade", "&reg");
-
-Console.WriteLine($"Quantity: {quantity}");
-Console.WriteLine($"Output: {output}");
+var date2 = new DateTime(2024, 01, 31);
+Console.WriteLine(date2.AddMonths(1).Ticks);
