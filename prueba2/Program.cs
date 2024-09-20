b368517cee715fd2232667d2af8598fb37646341
Sergio Lillo <sergiolillom@gmail.com>
2024-09-20 13:17:07 +0200

    Exercise 1 Lesson 12 IndexOf and Substring

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -7,30 +7,21 @@ using System.Text;
 using System.Globalization;
 CultureInfo.CurrentCulture = new CultureInfo("en-US");
 
-//Lesson 11 Array Methods - String interpolation
-//Exercise 2
-string customerName = "Ms. Barros";
+//Lesson 12 String Methods - IndexOf and Substring
+//Exercise 1
+const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
 
-string currentProduct = "Magic Yield";
-int currentShares = 2975000;
-decimal currentReturn = 0.1275m;
-decimal currentProfit = 55000000.0m;
+string quantity = "";
+string output = "";
 
-string newProduct = "Glorious Future";
-decimal newReturn = 0.13125m;
-decimal newProfit = 63000000.0m;
+var indexOfSpan = input.IndexOf("<span>");
+var lastIndexOfSpan = input.IndexOf("</span>");
+var length = lastIndexOfSpan - (indexOfSpan+6);
+quantity = input.Substring(indexOfSpan+6,length);
+var indexOfDiv = input.IndexOf("<div>");
+var lastIndexOfDiv = input.IndexOf("</div>");
+var length2 = lastIndexOfDiv - (indexOfDiv+5);
+output = input.Substring(indexOfDiv+5,length2).Replace("&trade", "&reg");
 
-Console.WriteLine($"Dear {customerName},");
-Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
-Console.WriteLine($"Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.\n");
-Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
-Console.WriteLine("Here's a quick comparison:\n");
-
-string currentProductAligned = currentProduct.PadRight(20);
-string newProductAligned = newProduct.PadRight(20);
-string currentReturnAligned = string.Format("{0:P2}", currentReturn).PadRight(10);
-string newReturnAligned = string.Format("{0:P2}", newReturn).PadRight(10);
-
-Console.WriteLine($"{currentProductAligned}{currentReturnAligned}{currentProfit:C}\n{newProductAligned}{newReturnAligned}{newProfit:C}");
-
-
+Console.WriteLine($"Quantity: {quantity}");
+Console.WriteLine($"Output: {output}");
