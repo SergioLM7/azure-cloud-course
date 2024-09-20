41548a171884f41518f10ef4c88e04d9672f2eb5
Sergio Lillo <sergiolillom@gmail.com>
2024-09-20 11:48:28 +0200

    Exercise 2 Lesson 11 Array Methods String Interpolation

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -5,102 +5,32 @@ using System.Collections.ObjectModel;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Globalization;
-CultureInfo.CurrentCulture = new CultureInfo("es-ES");
+CultureInfo.CurrentCulture = new CultureInfo("en-US");
 
-//Lesson 10 Do While
-//Exercise 1
-
-/*int userNumber = 0;
-string? userInput;
-Console.WriteLine("Please, introduce a number between 5 and 10.");
-
-do
-{
-    userInput = Console.ReadLine();
-    bool isInt = int.TryParse(userInput, out int numericValue);
-    if (!isInt)
-    {
-        Console.WriteLine("Sorry, you entered an invalid number, please try again.");
-    }
-    else
-    {
-        if (numericValue < 5 || numericValue > 10)
-        {
-            Console.WriteLine($"You entered {numericValue}. Please enter a number between 5 and 10.");
-        }
-        else
-        {
-            Console.WriteLine($"Your input value {numericValue} has been accepted.");
-            Environment.Exit(0);
-        }
-    }
-
-
-} while (userNumber > 10 || userNumber < 5);
-
+//Lesson 11 Array Methods - String interpolation
 //Exercise 2
+string customerName = "Ms. Barros";
 
-string? userRole = "";
-string? userInput2;
-Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
 
-do
-{
-    userInput2 = Console.ReadLine();
-    if (userInput2 != null)
-    {
-        userRole = userInput2.Trim();
-        if (userRole == "Administrator" || userRole == "Manager" || userRole == "User")
-        {
-            Console.WriteLine($"Your input value {userInput2} has been accepted.");
-             Environment.Exit(0);
-        }
-        else
-        {
-            Console.WriteLine($"The role name that you entered, {userRole} is not valid. Enter your role name (Administrator, Manager, or User)");
-        }
-    } else {
-        Console.WriteLine($"Your input value must be a string.");
-    }
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
 
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N2} shares at a return of {currentReturn:P2}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
+Console.WriteLine("Here's a quick comparison:\n");
 
+string currentProductAligned = currentProduct.PadRight(20);
+string newProductAligned = newProduct.PadRight(20);
+string currentReturnAligned = String.Format("{0:P2}", currentReturn).PadRight(10);
+string newReturnAligned = String.Format("{0:P2}", newReturn).PadRight(10);
 
+Console.WriteLine($"{currentProductAligned}{currentReturnAligned}{currentProfit:C}\n{newProductAligned}{newReturnAligned}{newProfit:C}");
 
-} while (userRole != "Administrator" || userRole != "Manager" || userRole != "User");
-*/
-//Exercise 3
-string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
-int stringsCount = myStrings.Length;
-
-string myString = "";
-int periodLocation = 0;
-
-for (int i = 0; i < stringsCount; i++)
-{
-    myString = myStrings[i];
-    periodLocation = myString.IndexOf(".");
-
-    string mySentence;
-
-    while (periodLocation != -1)
-    {
-
-        // first sentence is the string value to the left of the period location
-        mySentence = myString.Remove(periodLocation);
-
-        // the remainder of myString is the string value to the right of the location
-        myString = myString.Substring(periodLocation + 1);
-
-        // remove any leading white-space from myString
-        myString = myString.TrimStart();
-
-        // update the comma location and increment the counter
-        periodLocation = myString.IndexOf(".");
-
-        Console.WriteLine(mySentence);
-    }
- 
-    mySentence = myString.Trim();
-    Console.WriteLine(mySentence);
-}
 
