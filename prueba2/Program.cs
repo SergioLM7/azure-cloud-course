a2baaa46366294b76aaa5367b65466083072571d
SergioLM7 <sergiolillom@gmail.com>
2024-09-27 13:14:30 +0200

    Lesson 13 Exercise 4 TellFortune

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -8,69 +8,22 @@ using System.Globalization;
 CultureInfo.CurrentCulture = new CultureInfo("es-ES");
 
 //Lesson 13 Methods
-//Exercise 3
-/*
-if ipAddress consists of 4 numbers
-and
-if each ipAddress number has no leading zeroes
-and
-if each ipAddress number is in range 0 - 255
+//Exercise 4
+string[] text = { "You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to" };
+string[] good = { "look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!" };
+string[] bad = { "fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life." };
+string[] neutral = { "appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature." };
 
-then ipAddress is valid
+tellFortune();
 
-else ipAddress is invalid
-*/
-string[] ipv4Input = {"107.31.1.5", "255.0.0.255", "555..0.555", "255...255", };
-
-bool validLength;
-bool validZeroes = false;
-bool validRange = false;
-
-foreach (string ip in ipv4Input) 
+void tellFortune()
 {
-    string[] address = ip.Split(".", StringSplitOptions.RemoveEmptyEntries);
-
-    ValidateLength(address); 
-    ValidateZeroes(address); 
-    ValidateRange(address);
-
-    if (validLength && validZeroes && validRange) 
+    Random random = new Random();
+    int luck = random.Next(100);
+    Console.WriteLine("A fortune teller whispers the following words:");
+    string[] fortune = luck > 75 ? good : (luck < 25 ? bad : neutral);
+    for (int i = 0; i < 4; i++)
     {
-        Console.WriteLine($"{ip} is a valid IPv4 address");
-    } 
-    else 
-    {
-        Console.WriteLine($"{ip} is an INVALID IPv4 address");
+        Console.Write($"{text[i]} {fortune[i]} ");
     }
-}
-
-bool ValidateLength(string[] address) {
-    return validLength = address.Length == 4;
-};
-
-bool ValidateZeroes(string[] address) {
-
-    foreach (string number in address) 
-    {
-        if (number.Length > 1 && number.StartsWith("0")) 
-        {
-            return validZeroes = false;
-        }
-    }
-    return validZeroes = true;
-
-};
-
-bool ValidateRange(string[] address) {
-
-    foreach (string number in address) 
-    {
-        int value = int.Parse(number);
-        if (value < 0 || value > 255) 
-        {
-            return validRange = false;
-            
-        }
-    }
-    return validRange = true;
 };
