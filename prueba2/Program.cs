1a2774a20d5f8ed0523f49f9d59164ddbdfcaa09
SergioLM7 <sergiolillom@gmail.com>
2024-10-07 21:28:08 +0200

    Lesson 14 Final Challenge OK

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -11,49 +11,210 @@ using Microsoft.VisualBasic;
 CultureInfo.CurrentCulture = new CultureInfo("es-ES");
 
 //Lesson 14 Methods
-//Exercise - Dice Game
+//Exercise - Challenge - Game
+/*- The code declares the following variables:
+    - Variables to determine the size of the Terminal window.
+    - Variables to track the locations of the player and food.
+    - Arrays `states` and `foods` to provide available player and food appearances
+    - Variables to track the current player and food appearance
+
+- The code provides the following methods:
+    - A method to determine if the Terminal window was resized.
+    - A method to display a random food appearance at a random location.
+    - A method that changes the player appearance to match the food consumed.
+    - A method that temporarily freezes the player movement.
+    - A method that moves the player according to directional input.
+    - A method that sets up the initial game state.
+
+- The code doesn't call the methods correctly to make the game playable. The following features are missing:
+    - Code to determine if the player has consumed the food displayed.
+    - Code to determine if the food consumed should freeze player movement.
+    - Code to determine if the food consumed should increase player movement.
+    - Code to increase movement speed.
+    - Code to redisplay the food after it's consumed by the player.
+    - Code to terminate execution if an unsupported key is entered.
+    - Code to terminate execution if the terminal was resized.*/
 Random random = new Random();
 
-Console.WriteLine("Would you like to play? (Y/N)");
-var key = Console.ReadKey(true);
+Console.CursorVisible = false;
+int height = Console.WindowHeight - 1;
+int width = Console.WindowWidth - 5;
+bool shouldExit = false;
 
-if (ShouldPlay(key.KeyChar)) 
+// Console position of the player
+int playerX = 0;
+int playerY = 0;
+
+// Console position of the food
+int foodX = 0;
+int foodY = 0;
+
+// Available player and food strings
+string[] states = {"('-')", "(^-^)", "(X_X)"};
+string[] foods = {"@@@@@", "$$$$$", "#####"};
+
+// Current player string displayed in the Console
+string player = states[0];
+
+// Index of the current food
+int food = 0;
+
+InitializeGame();
+while (!shouldExit) 
 {
-    PlayGame();
-}
-
-string WinOrLose(int target, int roll)
-{
-    return target > roll ? "You lose!" : "You win!";
-}
-
-void PlayGame() 
-{
-    var play = true;
-
-    while (play) 
-    {
-        var target = random.Next(1,6);
-        var roll = random.Next(1,7);
-
-        Console.WriteLine($"Roll a number greater than {target} to win!");
-        Console.WriteLine("Roll your six-faces dice with the Enter key...");
-        Console.ReadLine();
-        Console.WriteLine($"You rolled a {roll}");
-        Console.WriteLine(WinOrLose(target, roll));
-        Console.WriteLine("\nPlay again? (Y/N)");
-        var key = Console.ReadKey(true);
-        play = ShouldPlay(key.KeyChar);
+    if(TerminalResized()) {
+        Console.Clear();
+        Console.WriteLine("Console was resized. Program exiting.");
+        System.Threading.Thread.Sleep(2000);
+        break;
+    } else {
+        Move();
     }
 }
 
-bool ShouldPlay(char key) 
+// Returns true if the Terminal was resized 
+bool TerminalResized() 
 {
-    if (key == 'y') {
+    return height != Console.WindowHeight - 1 || width != Console.WindowWidth - 5;
+}
+
+// Displays random food at a random location
+void ShowFood() 
+{
+    // Update food to a random index
+    food = random.Next(0, foods.Length);
+
+    // Update food position to a random location
+    foodX = random.Next(0, width - player.Length);
+    foodY = random.Next(0, height - 1);
+
+    // Display the food at the location
+    Console.SetCursorPosition(foodX, foodY);
+    Console.Write(foods[food]);
+}
+
+// Changes the player to match the food consumed
+void ChangePlayer() 
+{
+    player = states[food];
+    Console.SetCursorPosition(playerX, playerY);
+    Console.Write(player);
+}
+
+// Check if the player has eaten the food
+bool FoodEaten() 
+{
+    if (foodX == playerX) {
         return true;
-    } else if (key == 'n') {
-        return false;
     } else {
         return false;
     }
+}
+
+// Temporarily stops the player from moving
+void FreezePlayer() 
+{
+    System.Threading.Thread.Sleep(1000);
+}
+
+//Check if the player has the X_X aspect
+bool CheckPlayerAspect()
+{
+     if (player == "(X_X)") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Check if the player has the  (^-^) aspect
+bool CheckPlayerAspect2()
+{
+     if (player == "(^-^)") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Reads directional input from the Console and moves the player
+void Move(bool optional = false, bool moreSpeed = false) 
+{
+    int lastX = playerX;
+    int lastY = playerY;
+    var keyPulsed = Console.ReadKey(true).Key;
+
+    if(optional) {
+        if(keyPulsed != ConsoleKey.UpArrow && keyPulsed != ConsoleKey.DownArrow && keyPulsed != ConsoleKey.RightArrow && keyPulsed != ConsoleKey.LeftArrow) {
+            shouldExit = true;
+        }
+    }
+
+    if (CheckPlayerAspect()) {
+        FreezePlayer();
+    }
+
+    if (CheckPlayerAspect2()) {
+        moreSpeed = true;
+    }
+
+    if (moreSpeed) {
+        switch (keyPulsed) 
+    {
+		case ConsoleKey.LeftArrow:  
+            playerX-= 2; 
+            break;
+		case ConsoleKey.RightArrow: 
+            playerX+= 2; 
+            break;
+    }
+    }
+
+    switch (keyPulsed) 
+    {
+        case ConsoleKey.UpArrow:
+            playerY--; 
+            break;
+		case ConsoleKey.DownArrow: 
+            playerY++; 
+            break;
+		case ConsoleKey.LeftArrow:  
+            playerX--; 
+            break;
+		case ConsoleKey.RightArrow: 
+            playerX++; 
+            break;
+		case ConsoleKey.Escape:     
+            shouldExit = true; 
+            break;
+    }
+
+    // Clear the characters at the previous position
+    Console.SetCursorPosition(lastX, lastY);
+    for (int i = 0; i < player.Length; i++) 
+    {
+        Console.Write(" ");
+    }
+
+    // Keep player position within the bounds of the Terminal window
+    playerX = (playerX < 0) ? 0 : (playerX >= width ? width : playerX);
+    playerY = (playerY < 0) ? 0 : (playerY >= height ? height : playerY);
+
+    // Draw the player at the new location
+    Console.SetCursorPosition(playerX, playerY);
+    Console.Write(player);
+    if (FoodEaten()) {
+        ChangePlayer();
+        ShowFood();
+    } 
+
+}
+
+// Clears the console, displays the food and player
+void InitializeGame() 
+{
+    Console.Clear();
+    ShowFood();
+    Console.SetCursorPosition(0, 0);
+    Console.Write(player);
 }
