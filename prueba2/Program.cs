9eae1ebcd3e2079b03cf1e6d74ddc97d29321d21
Sergio Lillo <sergiolillom@gmail.com>
2024-10-09 11:10:39 +0200

    primer CI

--- prueba2/Program.cs
+++ prueba2/Program.cs
@@ -1,4 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//Mi primer CI
+// See https://aka.ms/new-console-template for more information
 using System;
 using System.Collections;
 using System.Collections.ObjectModel;
